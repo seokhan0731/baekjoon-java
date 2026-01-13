f6a035822a008dc512aba6d1849463c51503179b
Seokhwan Kim <wistin@naver.com>
2026-01-13 15:15:46 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -4,45 +4,86 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
-
     static boolean isDecimal(int number) {
         int i = 2;
-        int count = 0;
-        while (i <= number) {
+        while (i * i <= number) {
             if (number % i == 0) {
-                count++;
+                return false;
             }
             i++;
         }
-        if (count == 1) {
-            return true;
-        }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
 //        System.setIn(new FileInputStream("input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] numArray = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            numArray[i] = Integer.parseInt(st.nextToken());
-        }
-
         int decimalCount = 0;
         for (int i = 0; i < n; i++) {
-            if (isDecimal(numArray[i])) {
+            int number = Integer.parseInt(st.nextToken());
+            if (number < 2) {
+                continue;
+            }
+            if (isDecimal(number)) {
                 decimalCount++;
             }
         }
-
         sb.append(decimalCount);
         System.out.println(sb);
     }
 }
+
+
+/**
+ * 기존 코드
+ * 소수 판단 로직
+ * 2부터 자기 자신까지 while 루프를 통해, 약수 개수 판단
+ * 모든 숫자를 끝까지 루프를 돌아야하므로, 입력이 큰 숫자가 들어오면 비효율적!
+ *
+ * @param number 입력받은 단일 숫자
+ * @return 소수 여부
+ */
+//public class Main {
+//    static boolean isDecimal(int number) {
+//        int i = 2;
+//        int count = 0;
+//        while (i <= number) {
+//            if (number % i == 0) {
+//                count++;
+//            }
+//            i++;
+//        }
+//        if (count == 1) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+/// /        System.setIn(new FileInputStream("input.txt"));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int n = Integer.parseInt(br.readLine());
+//        int[] numArray = new int[n];
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        for (int i = 0; i < n; i++) {
+//            numArray[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        int decimalCount = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (isDecimal(numArray[i])) {
+//                decimalCount++;
+//            }
+//        }
+//
+//        sb.append(decimalCount);
+//        System.out.println(sb);
+//    }
+//}
