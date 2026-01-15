6fa265f5408e1026a49f40db97892621fd1a51ce
Seokhwan Kim <wistin@naver.com>
2026-01-15 13:29:26 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -5,34 +5,25 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static Vector<Integer> makeSumVector(int[] arr) {
-        Vector<Integer> sumVector = new Vector<>();
+    static int findMax(int[] arr, int goal) {
         int length = arr.length;
+        int sum = 0;
+        int max = 0;
         for (int i = 0; i < length - 2; i++) {
             for (int k = i + 1; k < length - 1; k++) {
                 for (int j = k + 1; j < length; j++) {
-                    sumVector.add(arr[i] + arr[k] + arr[j]);
+                    sum = arr[i] + arr[k] + arr[j];
+                    if (max < sum && sum <= goal) {
+                        max = sum;
+                    }
                 }
             }
         }
-        Collections.sort(sumVector);
-        return sumVector;
-    }
-
-    static int findSumClosetToGoal(Vector<Integer> sumVector, int goal) {
-        int i = sumVector.size();
-        while (true) {
-            if (goal >= sumVector.get(i - 1)) {
-                break;
-            }
-            i--;
-        }
-        return sumVector.get(i - 1);
+        return max;
     }
 
     public static void main(String[] args) throws IOException {
-//        System.setIn(new FileInputStream("input.txt"));
+        //System.setIn(new FileInputStream("input.txt"));
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -46,9 +37,9 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Vector<Integer> sumVector = makeSumVector(arr);
-        int sumClosetToGoal = findSumClosetToGoal(sumVector, m);
-        sb.append(sumClosetToGoal);
+
+        int max = findMax(arr, m);
+        sb.append(max);
         System.out.println(sb);
     }
 }
