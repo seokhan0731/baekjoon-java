a6eb743a62b0e21629e8bd22a5d40644c8226242
Seokhwan Kim <wistin@naver.com>
2026-01-21 13:32:24 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 604 ms, Memory: 50444 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -5,9 +5,13 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 코드 2. 비교 연산에 compare 메소드 이용
+ * 반환형의 경우는 부호값 변화는 동일
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
-        //System.setIn(new FileInputStream("input.txt"));
+//        System.setIn(new FileInputStream("input.txt"));
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,12 +26,12 @@ public class Main {
 
         Arrays.sort(array, (a, b) -> {
             if (a[0] != b[0]) {
-                return a[0] - b[0];
+                return Integer.compare(a[0], b[0]);
             }
-            return a[1] - b[1];
+            return Integer.compare(a[1], b[1]);
         });
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             sb.append(array[i][0]).append(' ')
                     .append(array[i][1]).append('\n');
         }
@@ -35,3 +39,40 @@ public class Main {
         System.out.println(sb);
     }
 }
+
+/**
+ * 기존 코드
+ * 정수 오버플로우를 고려하지 않고 정렬 기준 람다식 작성시, 두 수의 차 이용
+ * ->극단적으로 int 자료형의 범위가 차를 했을 때, 넘어간다면 의도하지 않는 결과 출력 가능성 존재
+ * => compare 메소드 사용 습관화 필요
+ */
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        System.setIn(new FileInputStream("input.txt"));
+//        StringBuilder sb = new StringBuilder();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int n = Integer.parseInt(br.readLine());
+//        int[][] array = new int[n][2];
+//
+//        for (int i = 0; i < n; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            array[i][0] = Integer.parseInt(st.nextToken());
+//            array[i][1] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        Arrays.sort(array, (a, b) -> {
+//            if (a[0] != b[0]) {
+//                return a[0] - b[0];
+//            }
+//            return a[1] - b[1];
+//        });
+//
+//        for(int i=0;i<n;i++){
+//            sb.append(array[i][0]).append(' ')
+//                    .append(array[i][1]).append('\n');
+//        }
+//
+//        System.out.println(sb);
+//    }
+//}
